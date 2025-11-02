08732a6007e945f95ce955c1b724ed380e34a930
Sandeep-kaur162 <23bcs14252@cuchd.in>
2025-11-02 18:08:40 +0530

    Create App.jsx

---
+++ FS_INTEGRATION/practice 3/App.jsx
@@ -0,0 +1,11 @@
+
+import React from 'react';
+import ChatRoom from './ChatRoom';
+
+export default function App() {
+  return (
+    <div style={{display:'flex', justifyContent:'center', padding:20}}>
+      <ChatRoom />
+    </div>
+  );
+}
